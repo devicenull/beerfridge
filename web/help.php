679844a43790d82f09a1c3cb31d6cd94327c24b0
Brian Rak <dn@devicenull.org>
2020-11-02 19:25:33 -0500

    Make barcode scanner use javascript keydown events rather then relying on an input field

--- web/help.php
+++ web/help.php
@@ -3,10 +3,12 @@ require(__DIR__.'/../init.php');
 
 $vars = [
 	'commands' => json_encode([
+		// barcodes must be strings because.. javascript
 		[BARCODE_MODE_ADD, 'Add Beer'],
 		[BARCODE_MODE_DEFAULT, 'Normal Mode'],
 		[BARCODE_ADD6_LASTBEER, 'Add 6 to last beer scanned'],
 		[BARCODE_ADD12_LASTBEER, 'Add 12 to last beer scanned'],
+		["123456", 'Test Beer'],
 	]),
 ];
 
